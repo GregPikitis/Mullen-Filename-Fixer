55a63aa9570c9504215e4d53bdbd9faba44eea5d
Matthew Griffin <matthew.griffin002@gmail.com>
2015-10-07 19:23:56 -0400

    Updated fileRename.py
    Added a class, some comments, and now lastName is whatever the users last name is.

--- fileRename.py
+++ fileRename.py
@@ -1,15 +1,37 @@
-import os
-import shutil
+# !/usr/bin/python
+#
+# Programmers: Hunter Harris, Matthew Griffin
+#
+# Description: A program that renames a file for our English teacher, Mrs.Mullen-Barnett
+#
+# Importing stuff
+try:
+	import os
+	import shutil
 
-olddir = "E:/School/2015-2016/English 2H/Raw/"
-newdir = "E:/School/2015-2016/English 2H/Mullen/"
+except ImportError:
+	print("Python is not installed correctly. Try re-installing.")
 
-print ("Preparing to rename files")
+class Rename:
+	def __init__(self):
+		# Making some basic variables, olddir, newdir, and lastName
+		olddir = "/home/matthew/Documents/Python/Mullen-Filename-Fixer/old"
+		newdir = "/home/matthew/Documents/Python/Mullen-Filename-Fixer/new"
 
-for filename in os.listdir(olddir):
-    print (filename)
-    if filename.endswith(".docx"):
-        shutil.copyfile(olddir + filename, newdir + filename)
-        os.rename(newdir + filename, newdir + "HARRIS_" + filename)
+		# Getting the last name, and editing the name a bit	
+		lastName = raw_input("What is your last name [i.e. 'Harris']?: ")
+		lastName = lastName.upper()
+		lastName = lastName + "_"
 
+		for filename in os.listdir(olddir):	
+			print ("FOUND FILE: " + filename) # Printing the name[s] of the file[s] in the 'old' dir
+			print("--Renaming file " + filename + "...")	
+	
+			# If the filename ends in '.docx' (MS Word)
+			if filename.endswith(".docx"):
+        			shutil.copyfile(olddir + "/" + filename, newdir + "/" + filename) # Copying the old file to the new dir
+        			os.rename(newdir + "/" + filename, newdir + "/" + lastName + filename) # Renaming the file
+			print("FILE RENAMING DONE!")
 
+if(__name__ == "__main__"):
+	rename = Rename()
